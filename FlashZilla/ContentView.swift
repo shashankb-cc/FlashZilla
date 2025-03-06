05fa7bf2a8a7a76a4b96ad43f7d3c3f5f7c2160d
shashankb-cc <shashank.b@codecraft.co.in>
2025-03-06 11:14:17 +0530

    Initial Commit

---
+++ FlashZilla/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlashZilla
+//
+//  Created by Shashank B on 06/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Shashank B on 06/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
